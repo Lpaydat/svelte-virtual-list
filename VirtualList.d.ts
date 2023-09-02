6dd57ad636382f0c6e3eba95c543c75f9dd5ea79
Prathak Paisanwatcharakit <lpaydat@gmail.com>
2023-09-03 01:22:58 +0700

    add type file and upgrade dependencies to latest

---
+++ VirtualList.d.ts
@@ -0,0 +1,30 @@
+declare module "VirtualList" {
+  import { SvelteComponent } from "svelte";
+
+  interface VirtualListProps {
+    items: Array<any>;
+    height?: string | number;
+    itemHeight?: number;
+    start?: number;
+    end?: number;
+    isAtTop?: boolean;
+    isAtBottom?: boolean;
+  }
+
+  interface VirtualListMethods {
+    scrollToIndex(index: number, opts?: ScrollToOptions): Promise<void>;
+    scrollToBottom(shouldStickToBottom?: boolean): Promise<void>;
+  }
+
+  interface VirtualListEvents {
+    scroll: CustomEvent<void>;
+  }
+
+  class VirtualList extends SvelteComponent<
+    VirtualListProps,
+    VirtualListEvents,
+    VirtualListMethods
+  > {}
+
+  export default VirtualList;
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,22 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import pkg from './package.json';
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import pkg from "./package.json";
 
 export default [
-	{
-		input: 'test/src/index.js',
-		output: { file: 'test/public/bundle.js', 'format': 'iife' },
-		plugins: [
-			resolve(),
-			commonjs(),
-			svelte()
-		]
-	},
+  {
+    input: "test/src/index.js",
+    output: { file: "test/public/bundle.js", format: "iife" },
+    plugins: [resolve(), commonjs(), svelte()],
+  },
 
-	// tests
-	{
-		input: 'test/src/index.js',
-		output: {
-			file: 'test/public/bundle.js',
-			format: 'iife'
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-			svelte()
-		]
-	}
+  // tests
+  {
+    input: "test/src/index.js",
+    output: {
+      file: "test/public/bundle.js",
+      format: "iife",
+    },
+    plugins: [resolve(), commonjs(), svelte()],
+  },
 ];
